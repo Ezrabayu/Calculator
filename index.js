5b6502a9aaef3af4bbddb6d0b86549df24ddbce3
Ezrabayu <ezrabayu0@gmail.com>
2024-11-21 00:37:22 +0700

    Remove Comment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input) {
-    // Prevent multiple consecutive operators
     if (input === "+" || input === "-" || input === "*" || input === "/") {
         const lastChar = display.value.slice(-1);
         if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
-            return; // Ignore input if the last character is already an operator
+            return; 
         }
 
-        // Prevent operator at the start of the input
         if (display.value === "" && (input === "+" || input === "-" || input === "*" || input === "/")) {
             return;
         }
@@ -22,7 +20,6 @@ function clearDisplay() {
 
 function calculate() {
     try {
-        // Safely evaluate the expression
         display.value = eval(display.value);
     }
     catch(error) {
@@ -31,5 +28,5 @@ function calculate() {
 }
 
 function deleteLastCharacter() {
-    display.value = display.value.slice(0, -1); // Hapus karakter terakhir
+    display.value = display.value.slice(0, -1); 
 }
